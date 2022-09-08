c5107d672911fcb594c839ed3c734f247aa15794
Pete Fowler <fowler.pete@gmail.com>
2022-09-08 14:18:19 -0600

    Initial project setup

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+
+  render () {
+    return (
+      <div id='header'>
+        CV Builder
+      </div>
+    )
+  }
+}
+
+export default Header;
